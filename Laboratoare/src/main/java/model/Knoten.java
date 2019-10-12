3bfae1c534935092ccd2f47522eaffc820877fc6
Sonya <sonyaparau@yahoo.com>
2019-10-12 15:33:13 +0300

    Model des Projekts

---
+++ Laboratoare/src/main/java/model/Knoten.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Knoten {
+
+    private Integer id;
+    private String name;
+
+    public Knoten(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
